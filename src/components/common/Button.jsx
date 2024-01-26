2f057b67f3fe4cfdb12a83ba0b5a6e8694d55c4c
Somya Gupta <somyag00@gmail.com>
2024-01-26 12:11:31 -0500

    Updated caniuse-lite package

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({text, linkto}) => {
   return (
     <button className='bg-[#e84949] transition-all duration-200 ease-in-out transform cursor-pointer hover:scale-105 text-white py-4 px-4 h-[4rem] w-[10rem] rounded-md text-lg font-semibold border-10'>
-        <a href={linkto} target='_blank'>{text}</a>
+        <a href={linkto} target='_blank' rel='noreferrer'>{text}</a>
     </button>
   )
 }
